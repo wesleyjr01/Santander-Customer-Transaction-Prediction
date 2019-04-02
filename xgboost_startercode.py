ace2dbb4ef8b54c168c98de3dc28055133913a9d
Wesley Bortolozo <wesleyjr0101@gmail.com>
2019-04-02 16:55:17 -0300

    SVCLinear model

--- xgboost_startercode.py
+++ xgboost_startercode.py
@@ -15,10 +15,10 @@ test.drop(['ID_code'], axis=1, inplace=True)
 
 
 MAX_TREE_DEPTH = 2
-LEARNING_RATE = 0.0067
+LEARNING_RATE = 0.017
 TREE_METHOD = 'hist'
 ITERATIONS = 100000
-SUBSAMPLE = 0.67
+SUBSAMPLE = 0.57
 REGULARIZATION = 0.23
 GAMMA = 0.3
 POS_WEIGHT = 1
@@ -31,35 +31,35 @@ params = {'tree_method': TREE_METHOD, 'max_depth': MAX_TREE_DEPTH, 'alpha': REGU
 
 result = np.zeros(test.shape[0])  # Vector to be filled
 oof = np.zeros(len(train))  # Vector to be filled
+for splits in [2, 4, 6]:
+  rskf = RepeatedStratifiedKFold(n_splits=splits, n_repeats=3, random_state=17)
+  for counter, (train_index, valid_index) in enumerate(rskf.split(train, train.target), 1):
+      print(counter)
 
-rskf = RepeatedStratifiedKFold(n_splits=2, n_repeats=1, random_state=17)
-for counter, (train_index, valid_index) in enumerate(rskf.split(train, train.target), 1):
-    print(counter)
+      # Train data
+      t = train.iloc[train_index]
+      trn_data = xgb.DMatrix(t.drop('target', axis=1).values,
+                             t.target.values)
 
-    # Train data
-    t = train.iloc[train_index]
-    trn_data = xgb.DMatrix(t.drop('target', axis=1).values,
-                           t.target.values)
+      # Validation data
+      v = train.iloc[valid_index]
+      val_data = xgb.DMatrix(v.drop('target', axis=1).values,
+                             v.target.values)
 
-    # Validation data
-    v = train.iloc[valid_index]
-    val_data = xgb.DMatrix(v.drop('target', axis=1).values,
-                           v.target.values)
+      # Training
+      model = xgb.train(params, trn_data, ITERATIONS, evals=[(trn_data, "train"), (val_data, "eval")],
+                        early_stopping_rounds=EARLY_STOP, verbose_eval=1)
 
-    # Training
-    model = xgb.train(params, trn_data, ITERATIONS, evals=[(trn_data, "train"), (val_data, "eval")],
-                      early_stopping_rounds=EARLY_STOP, verbose_eval=1)
+      # Output .pkl model
+      model_name = "XGB_" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '.pkl'
+      model_name = model_name.replace(':', '_')
+      pickle.dump(model, open(model_name, 'wb'))
 
-    # Output .pkl model
-    model_name = "XGB_" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '.pkl'
-    model_name = model_name.replace(':', '_')
-    pickle.dump(model, open(model_name, 'wb'))
+      # Feed OOF Vector with Val Prediction
+      oof[valid_index] = model.predict(xgb.DMatrix(v.drop('target', axis=1).values))
 
-    # Feed OOF Vector with Val Prediction
-    oof[valid_index] = model.predict(xgb.DMatrix(v.drop('target', axis=1).values))
-
-    # Feed Test Prediction Vector
-    result += model.predict(xgb.DMatrix(test.values))
+      # Feed Test Prediction Vector
+      result += model.predict(xgb.DMatrix(test.values))
 
 submission = pd.DataFrame({"ID_code": test_id})
 submission['target'] = result / counter
