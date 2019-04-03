869c78394e5cf551f0286755e95d6aad13604e97
Wesley Bortolozo <wesleyjr0101@gmail.com>
2019-04-03 17:17:13 -0300

    att xgboost

--- xgboost_startercode.py
+++ xgboost_startercode.py
@@ -8,6 +8,18 @@ import xgboost as xgb
 import pickle
 import datetime
 
+def augment(train, num_n=1, num_p=2):
+    newtrain = [train]
+
+    n = train[train.target == 0]
+    for i in range(num_n):
+        newtrain.append(n.apply(lambda x: x.values.take(np.random.permutation(len(n)))))
+
+    for i in range(num_p):
+        p = train[train.target > 0]
+        newtrain.append(p.apply(lambda x: x.values.take(np.random.permutation(len(p)))))
+    return pd.concat(newtrain)
+
 train = pd.read_pickle("train.pkl").drop("ID_code", axis=1)
 test = pd.read_pickle("test.pkl")
 test_id = test['ID_code'].values
@@ -15,7 +27,7 @@ test.drop(['ID_code'], axis=1, inplace=True)
 
 
 MAX_TREE_DEPTH = 2
-LEARNING_RATE = 0.017
+LEARNING_RATE = 0.039
 TREE_METHOD = 'auto'
 ITERATIONS = 100000
 SUBSAMPLE = 0.57
@@ -27,13 +39,16 @@ MIN_CHILD_WEIGHT = 85
 params = {'tree_method': TREE_METHOD, 'max_depth': MAX_TREE_DEPTH, 'alpha': REGULARIZATION,
           'gamma': GAMMA, 'subsample': SUBSAMPLE, 'scale_pos_weight': POS_WEIGHT, 'learning_rate': LEARNING_RATE,
           'silent': 1, 'objective': 'binary:logistic', 'eval_metric': 'auc', 'silent': True,
-          'verbose_eval': False, 'min_child_weight' : MIN_CHILD_WEIGHT}
+          'verbose_eval': False, 'min_child_weight' : MIN_CHILD_WEIGHT,
+          'gpu_id' : 0, # FOR GPU
+          'max_bin' : 16, # FOR GPU
+          'tree_method': 'gpu_hist' # FOR GPU}
 
 
 result = np.zeros(test.shape[0])  # Vector to be filled
 oof = np.zeros(len(train))  # Vector to be filled
 seed = 17
-list_of_splits = [2, 3, 5]
+list_of_splits = [3, 5, 7, 9]
 n_repeats = 3
 for splits in list_of_splits:
   rskf = RepeatedStratifiedKFold(n_splits=splits, n_repeats=n_repeats, random_state=seed)
@@ -42,6 +57,7 @@ for splits in list_of_splits:
 
       # Train data
       t = train.iloc[train_index]
+      t = augment(t)
       trn_data = xgb.DMatrix(t.drop('target', axis=1).values,
                              t.target.values)
 
